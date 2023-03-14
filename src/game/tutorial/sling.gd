2b2701187123b9c0916e9841e96c32fc840f924c
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-03-14 10:53:08 +0100

    Set up fresh Godot project

---
+++ src/game/tutorial/sling.gd
@@ -0,0 +1,7 @@
+extends StaticBody2D
+
+func on_ball_entered(ball: RigidBody2D):
+	var direction = $SlingArea2D/CollisionShape2D.global_rotation
+	var baseForce = Vector2(1, 0).rotated(direction)
+	var impulse = baseForce * 1000
+	ball.apply_impulse(impulse)
