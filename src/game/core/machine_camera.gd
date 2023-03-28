6b96a5c4d7875efed6ced854a2b9ac5ba3d37df4
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-03-28 16:17:00 +0200

    Improve screen shake mechanics

--- src/game/core/machine_camera.gd
+++ src/game/core/machine_camera.gd
@@ -1,36 +1,38 @@
 extends Camera2D
 
-var shake_strength: float = 0.0
+var _time_pressure := Scoring.TimePressure.LOW
 
 func _ready():
 	Scoring.time_left_changed.connect(_on_time_left_changed)
 	Scoring.time_ran_out.connect(_on_time_ran_out)
-	_set_pressure(Scoring.TimePressure.LOW)
+	GameStore.level_changed.connect(_on_level_changed)
+
 
 func _process(delta: float) -> void:
-	var shake_offset := Vector2(
-		randf_range(-shake_strength, shake_strength),
-		randf_range(-shake_strength, shake_strength)
+	var strength: float = 0.0
+	
+	match _time_pressure:
+		Scoring.TimePressure.LOW:
+			return
+		Scoring.TimePressure.MEDIUM:
+			strength = 0.5
+		Scoring.TimePressure.HIGH:
+			strength = 1.5
+	
+	offset = Vector2(
+		randf_range(-strength, strength),
+		randf_range(-strength, strength)
 	)
-	offset = shake_offset
 
 
 func _on_time_left_changed(_to: float, pressure):
-	_set_pressure(pressure)
+	_time_pressure = pressure
 
 
 func _on_time_ran_out():
-	_set_pressure(Scoring.TimePressure.LOW)
+	_time_pressure = Scoring.TimePressure.LOW
 
 
-func _set_pressure(pressure):
-	match pressure:
-		Scoring.TimePressure.HIGH:
-			shake_strength = 2.5
-			process_mode = Node.PROCESS_MODE_INHERIT
-		Scoring.TimePressure.MEDIUM:
-			shake_strength = 0.5
-			process_mode = Node.PROCESS_MODE_INHERIT
-		_:
-			offset = Vector2(0, 0)
-			process_mode = Node.PROCESS_MODE_DISABLED
+func _on_level_changed(_level: int):
+	_time_pressure = Scoring.TimePressure.LOW
+
