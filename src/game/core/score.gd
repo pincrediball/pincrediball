38dc8cc0fe156faa0d1dca9722109240581e8695
Jeroen Heijmans <github@jeroenheijmans.nl>
2023-03-28 12:05:18 +0200

    Improved drain timer

--- src/game/core/score.gd
+++ src/game/core/score.gd
@@ -5,22 +5,25 @@ const HIGH_SCORE_SOUND = preload("res://sound/high-score-achieved-001.mp3")
 var _stage
 
 func _ready():
+	_stage = GameStore.get_current_stage()
+	
 	Scoring.score_changed.connect(_on_score_changed)
 	Scoring.scoring_mode_toggled.connect(_on_scoring_mode_toggled)
 	Scoring.time_left_changed.connect(_on_time_left_changed)
-	_on_time_left_changed(GameStore.get_current_stage().max_run_time)
-
-	_stage = GameStore.get_current_stage()
+	
 	GameStore.level_changed.connect(_on_level_changed)
 	GameStore.high_score_changed.connect(_on_high_score_changed)
+	
 	_set_score(0)
 	_set_high_score(GameStore.get_current_stage_high_score())
+	_set_time_left(_stage.max_run_time)
 
 
 func _on_level_changed(_level: int):
 	_stage = GameStore.get_current_stage()
 	_set_score(0)
 	_set_high_score(GameStore.get_current_stage_high_score())
+	_set_time_left(_stage.max_run_time)
 
 
 func _on_score_changed(_from: int, to: int):
@@ -37,7 +40,12 @@ func _on_scoring_mode_toggled(enabled: bool):
 
 
 func _on_time_left_changed(to: float):
+	_set_time_left(to)
+
+
+func _set_time_left(to: float):
 	%TimeLeftLabel.text = "%.3fs" % to
+	%ProgressBar.value = to / _stage.max_run_time * 100.0
 
 
 func _set_score(to: int):
